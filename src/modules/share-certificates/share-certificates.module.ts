f22a93c0b5f997fc49d834c2618740f35854d59c
Mahdi <mahdinurazaman@gmail.com>
2026-03-28 13:14:15 +0300

    backend v1

--- src/modules/share-certificates/share-certificates.module.ts
+++ src/modules/share-certificates/share-certificates.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyUsersModule } from '../company-users/company-users.module';
 import { ShareCertificate } from './entities/share-certificate.entity';
 import { ShareCertificatesController } from './share-certificates.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ShareCertificate])],
+  imports: [
+    TypeOrmModule.forFeature([ShareCertificate]),
+    CompanyUsersModule,
+  ],
   controllers: [ShareCertificatesController],
   exports: [TypeOrmModule],
 })
--- src/modules/share-transactions/share-transactions.module.ts
+++ src/modules/share-transactions/share-transactions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shareholder } from '../shareholders/entities/shareholder.entity';
 import { ShareTransaction } from './entities/share-transaction.entity';
 import { ShareTransactionsService } from './share-transactions.service';
 import { ShareTransactionsController } from './share-transactions.controller';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([ShareTransaction])],
+	imports: [TypeOrmModule.forFeature([ShareTransaction, Shareholder])],
 	exports: [TypeOrmModule],
 	providers: [ShareTransactionsService],
 	controllers: [ShareTransactionsController],
